d2fe98fa1e425c7578215433b20fc30346c1ca5e
yukyduky <yukyduky@users.noreply.github.com>
2018-03-25 16:52:28 +0200

    Filled out ComponentManager, seems to work

--- YEngine/ComponentManager.cpp
+++ YEngine/ComponentManager.cpp
@@ -1,43 +1,72 @@
 #include "ComponentManager.h"
 
 
-
 void ComponentManager::init()
 {
-
 }
 
-const size_t ComponentManager::addEntity(const size_t componentBitmask)
+size_t ComponentManager::addEntity(std::bitset<BITMASK_SIZE> componentBitmask)
 {
-	return componentBitmask;
+	size_t id =  m_Entities.push(componentBitmask);
+
+	this->addComponentsToEntity(id, componentBitmask);
+
+	return id;
 }
 
 void ComponentManager::removeEntity(const size_t entityID)
 {
+	this->removeComponentFromEntity(entityID, m_Entities[entityID]);
 
+	m_Entities.remove(entityID);
 }
 
-void ComponentManager::addComponentToEntity(const size_t entityID, const size_t componentBitmask)
+void ComponentManager::addComponentsToEntity(const size_t entityID, std::bitset<BITMASK_SIZE> componentBitmask)
 {
+	size_t componentsAdded = 0;
+	std::unordered_map<std::bitset<BITMASK_SIZE>, ComponentData>::iterator it = m_Components.begin();
 
+	while (it != m_Components.end() && componentsAdded < componentBitmask.count())
+	{
+		if (((*it).first & componentBitmask) == (*it).first)
+		{
+			componentsAdded++;
+
+			Component* component = nullptr;
+			(*it).second.mm.requestMemory(component, &(*it).second.templateInstance, (*it).second.byteSize);
+			(*it).second.components.push(component);
+		}
+
+		it++;
+	}
 }
 
-void ComponentManager::removeComponentFromEntity(const size_t entityID, const size_t componentBitmask)
+void ComponentManager::removeComponentFromEntity(const size_t entityID, const std::bitset<BITMASK_SIZE> componentBitmask)
 {
+	ComponentData& data = m_Components.at(componentBitmask);
 
+	// Release its memory to the MemoryManager
+	data.mm.releaseMemory(data.components[entityID]);
+	// Removed the component from the idlist
+	data.components.remove(entityID);
 }
 
-void ComponentManager::registerComponentType(Byte* templateComponent, size_t componentSize, size_t componentTypeID)
+void ComponentManager::registerComponentType(Component& templateInstance, size_t byteSize, const char* name, size_t maxCapacity, std::bitset<BITMASK_SIZE> componentBitmask)
 {
-
+	m_Components.insert(m_Components.end(), std::pair<std::bitset<BITMASK_SIZE>, ComponentData>(componentBitmask, ComponentData(templateInstance, name, byteSize, maxCapacity)));
 }
 
-void ComponentManager::unregisterComponentType(size_t componentTypeID)
+void ComponentManager::unregisterComponentType(std::bitset<BITMASK_SIZE> componentBitmask)
 {
-
+	m_Components.at(componentBitmask).mm.cleanup();
+	m_Components.erase(componentBitmask);
 }
 
 void ComponentManager::cleanup()
 {
-
+	for (auto &i : m_Components)
+	{
+		i.second.mm.cleanup();
+	}
+	m_Components.clear();
 }
--- YEngine/ComponentManager.h
+++ YEngine/ComponentManager.h
@@ -2,24 +2,53 @@
 #ifndef COMPONENTMANAGER_H
 #define COMPONENTMANAGER_H
 
+#include <map>
 #include <unordered_map>
-#include "YUtilityTypes.h"
+#include <bitset>
+#include "Component.h"
+#include "MemoryManager.h"
+#include "idlist.h"
+
+#define BITMASK_SIZE 32
+
+struct ComponentData
+{
+	MemoryManager mm;
+	idlist<Component*> components;
+
+	Component& templateInstance;
+	const char* name;
+	size_t byteSize;
+
+	ComponentData(Component& templateInstance, const char* name, size_t byteSize, size_t maxCapacity) :
+		templateInstance(templateInstance), name(name), byteSize(byteSize), mm(byteSize, maxCapacity), components(maxCapacity) {}
+};
 
 class ComponentManager
 {
 private:
-	std::unordered_map<size_t, size_t> m_EntityToBitmask;
+	idlist<std::bitset<BITMASK_SIZE>> m_Entities;
+	std::unordered_map<std::bitset<BITMASK_SIZE>, ComponentData> m_Components;
 public:
 	void init();
 
-	const size_t addEntity(const size_t componentBitmask);
+	size_t addEntity(std::bitset<BITMASK_SIZE> componentBitmask);
 	void removeEntity(const size_t entityID);
-	void addComponentToEntity(const size_t entityID, const size_t componentBitmask);
-	void removeComponentFromEntity(const size_t entityID, const size_t componentBitmask);
-	void registerComponentType(Byte* templateComponent, size_t componentSize, size_t componentTypeID);
-	void unregisterComponentType(size_t componentTypeID);
+	void addComponentsToEntity(const size_t entityID, std::bitset<BITMASK_SIZE> componentBitmask);
+	void removeComponentFromEntity(const size_t entityID, const std::bitset<BITMASK_SIZE> componentBitmask);
+	void registerComponentType(Component& templateInstance, size_t byteSize, const char* name, size_t maxCapacity, std::bitset<BITMASK_SIZE> componentBitmask);
+	void unregisterComponentType(std::bitset<BITMASK_SIZE> componentBitmask);
+
+	template<typename Derived>
+	void getComponentFromEntity(const size_t entityID, const std::bitset<BITMASK_SIZE> componentBitmask, Derived*& component);
 
 	void cleanup();
 };
 
 #endif // COMPONENTMANAGER_H
+
+template<typename Derived>
+inline void ComponentManager::getComponentFromEntity(const size_t entityID, const std::bitset<BITMASK_SIZE> componentBitmask, Derived*& component)
+{
+	component = static_cast<Derived*>(m_Components.at(componentBitmask).components[entityID]);
+}
