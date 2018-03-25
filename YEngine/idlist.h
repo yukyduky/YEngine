dd8cb4eaa4a2eb87a514ec39223611529da5fa8c
yukyduky <yukyduky@users.noreply.github.com>
2018-03-25 15:23:23 +0200

    Refactored idlist to work with continous memory

--- YEngine/idlist.h
+++ YEngine/idlist.h
@@ -2,90 +2,127 @@
 #ifndef IDLIST_H
 #define IDLIST_H
 
-#include "IDHandler.h"
+#include <list>
 #include <vector>
+#include <unordered_map>
+#include <queue>
+#include <functional>
 
 template<class T>
 class idlist
 {
 private:
-	IDHandler m_IDs;
+	std::list<size_t> m_AvailableIDs;
+	std::priority_queue<size_t, std::vector<size_t>, std::greater<size_t>> m_AvailableIndexes;
+	std::unordered_map<size_t, size_t> m_IDToIndex;
+	std::unordered_map<size_t, size_t> m_IndexToID;
 	std::vector<typename T> m_Container;
+	size_t m_NrOfElements;
 public:
 	idlist();
 	idlist(const size_t size);
 
-	void push(typename T element);
-	void remove(const size_t& id);
-	void remove(std::list<size_t>::const_iterator it);
-	void resize(const size_t size);
-	const size_t& capacity();
+	size_t push(typename T element);
+	void remove(const size_t id);
+	void reserve(const size_t size);
+	size_t capacity();
 	void clear();
-	bool getElement(const size_t& id, typename T& element);
+	T& getElementByID(const size_t id);
 	bool empty();
-	const size_t& peekNextID();
+	size_t size();
+	T* data();
+	size_t peekNextID();
 
-	std::list<size_t>::const_iterator begin() const;
-	std::list<size_t>::const_iterator end() const;
-	std::list<size_t>::const_reverse_iterator rbegin() const;
-	std::list<size_t>::const_reverse_iterator rend() const;
-
-	T& operator[](std::list<size_t>::const_iterator it);
+	T& operator[](size_t id);
 };
 
 template<class T>
-const size_t& idlist<T>::peekNextID()
-{
-	return m_IDs.peekNextID();
-}
-
-template<class T>
-inline idlist<T>::idlist() : m_IDs()
+inline idlist<T>::idlist() : m_NrOfElements(0)
 {
 }
 
 template<class T>
-inline idlist<T>::idlist(const size_t size) : m_IDs()
+inline idlist<T>::idlist(const size_t size) : idlist()
 {
-	this->resize(size);
+	this->reserve(size);
 }
 
 template<class T>
-inline void idlist<T>::push(typename T element)
+inline size_t idlist<T>::push(typename T element)
 {
-	size_t id = m_IDs.getNewID();
+	size_t index = m_NrOfElements;
+	size_t id = index;
 
-	if (id == m_Container.size()) {
+	if (m_AvailableIDs.empty()) 
+	{
 		m_Container.push_back(element);
+		m_IDToIndex.insert(m_IDToIndex.end(), std::pair<size_t, size_t>(id, index));
+		m_IndexToID.insert(m_IndexToID.end(), std::pair<size_t, size_t>(index, id));
 	}
-	else {
-		m_Container[id] = element;
+	else 
+	{
+		id = m_AvailableIDs.front();
+		m_AvailableIDs.pop_front();
+		index = m_AvailableIndexes.top();
+		m_AvailableIndexes.pop();
+
+		m_Container[index] = element;
+		m_IDToIndex[id] = index;
+		m_IndexToID[index] = id;
+	}
+
+	m_NrOfElements++;
+
+	return id;
+}
+
+template<class T>
+inline void idlist<T>::remove(const size_t id)
+{
+	size_t lastIndex = m_NrOfElements - 1;
+
+	if (lastIndex == 0)
+	{
+		m_IDToIndex.erase(id);
+		m_IndexToID.erase(lastIndex);
+		m_AvailableIDs.push_front(id);
+		m_AvailableIndexes.push(lastIndex);
+		m_NrOfElements--;
+	}
+	else if (lastIndex != static_cast<size_t>(-1))
+	{
+		// Get the index of the id to remove
+		size_t holeIndex = m_IDToIndex.at(id);
+
+		m_Container[holeIndex] = m_Container[lastIndex];
+
+		m_IDToIndex[m_IndexToID[lastIndex]] = holeIndex;
+		m_IndexToID[holeIndex] = m_IndexToID[lastIndex];
+
+		m_IDToIndex.erase(id);
+		m_IndexToID.erase(lastIndex);
+		m_AvailableIDs.push_front(id);
+		m_AvailableIndexes.push(lastIndex);
+		m_NrOfElements--;
 	}
 }
 
 template<class T>
-inline void idlist<T>::remove(const size_t& id)
+inline void idlist<T>::reserve(const size_t size)
 {
-	m_IDs.remove(id);
-}
+	m_Container.resize(size);
+	m_IDToIndex.reserve(size);
+	m_IndexToID.reserve(size);
 
-template<class T>
-inline void idlist<T>::remove(std::list<size_t>::const_iterator it)
-{
-	m_IDs.remove(it);
-}
-
-template<class T>
-inline void idlist<T>::resize(const size_t size)
-{
-	if (size > m_Container.size()) {
-		m_IDs.resize(size);
-		m_Container.resize(size);
+	for (size_t i = m_NrOfElements + m_AvailableIDs.size(); i < size; i++)
+	{
+		m_AvailableIDs.push_back(i);
+		m_AvailableIndexes.push(i);
 	}
 }
 
 template<class T>
-inline const size_t & idlist<T>::capacity()
+inline size_t idlist<T>::capacity()
 {
 	return m_Container.capacity();
 }
@@ -94,55 +131,57 @@ template<class T>
 inline void idlist<T>::clear()
 {
 	m_Container.clear();
-	m_IDs.clear();
+	m_IDToIndex.clear();
+	m_IndexToID.clear();
+	m_AvailableIDs.clear();
+
+	for (size_t i = 0; i < m_AvailableIndexes.size(); i++)
+	{
+		m_AvailableIndexes.pop();
+	}
+
+	m_NrOfElements = 0;
 }
 
 template<class T>
-inline bool idlist<T>::getElement(const size_t& id, typename T& element)
+inline T& idlist<T>::getElementByID(const size_t id)
 {
-	bool elementFound = false;
-	std::list<size_t>::const_iterator it;
-	if (m_IDs.idInUse(id)) {
-		elementFound = true;
-		element = m_Container[*it];
-	}
-	return elementFound;
+	return m_Container[m_IDToIndex.at(id)]; // TODOYE: Throw own exception
 }
 
 template<class T>
 inline bool idlist<T>::empty()
 {
-	return m_IDs.empty();
+	return m_IDToIndex.empty();
 }
 
 template<class T>
-inline std::list<size_t>::const_iterator idlist<T>::begin() const
+inline size_t idlist<T>::size()
 {
-	return m_IDs.begin();
+	return m_NrOfElements;
 }
 
 template<class T>
-inline std::list<size_t>::const_iterator idlist<T>::end() const
+inline T* idlist<T>::data()
 {
-	return m_IDs.end();
+	return m_Container.data();
 }
 
 template<class T>
-inline std::list<size_t>::const_reverse_iterator idlist<T>::rbegin() const
+inline size_t idlist<T>::peekNextID()
 {
-	return this->rbegin();
+	return m_AvailableIDs.front();
 }
 
 template<class T>
-inline std::list<size_t>::const_reverse_iterator idlist<T>::rend() const
+inline T& idlist<T>::operator[](size_t index)
 {
-	return m_IDs.rend();
-}
+	if (index >= m_NrOfElements)
+	{
+		throw("idlist - Subscript out of range");
+	}
 
-template<class T>
-inline T& idlist<T>::operator[](std::list<size_t>::const_iterator it)
-{
-	return m_Container[*it];
+	return m_Container[index];
 }
 
 #endif
