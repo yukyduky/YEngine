2bd4c6af923fa0b1afc58f75bba4bbafa6a2c4a2
yukyduky <yukyduky@users.noreply.github.com>
2018-03-29 17:59:21 +0200

    Added function for removing a resource completely

--- YEngine/IResourceManager.h
+++ YEngine/IResourceManager.h
@@ -4,44 +4,57 @@
 
 #include "Resource.h"
 
-class IResourceManager
+namespace YEngine
 {
-public:
-	//***********************************************************
-	// Method:    createResource
-	// FullName:  ResourceManager::createResource
-	// Access:    public 
-	// Returns:   bool
-	// Qualifier:
-	// Parameter: std::string filename
-	// Parameter: RESOURCETYPE type
-	// Parameter: size_t ID
-	// Description: Factory that creates the desired resource type. The resource can be retrieved using the ID.
-	//***********************************************************
-	virtual bool createResource(std::string filename, RESOURCETYPE::TYPE type, size_t ID) = 0;
-	//***********************************************************
-	// Method:    reloadResource
-	// FullName:  ResourceManager::reloadResource
-	// Access:    public 
-	// Returns:   bool
-	// Qualifier:
-	// Parameter: size_t ID
-	// Description: Reloads the resource with the given ID. Reloading is for loading a resource that has already been loaded before and has since been unloaded.
-	// It will attempt to load the resource using the filename given when it was loaded the first time.
-	//***********************************************************
-	virtual bool reloadResource(size_t ID) = 0;
-	//***********************************************************
-	// Method:    unloadResource
-	// FullName:  ResourceManager::unloadResource
-	// Access:    public 
-	// Returns:   void
-	// Qualifier:
-	// Parameter: size_t ID
-	// Description: Unloads the resource with the given ID. Unloading is for releasing the resources used, back to the system.
-	//***********************************************************
-	virtual void unloadResource(size_t ID) = 0;
-	virtual bool isResourceLoaded(size_t ID) = 0;
-	virtual void cleanup() = 0;
-};
+	class IResourceManager
+	{
+	public:
+		//***********************************************************
+		// Method:    createResource
+		// FullName:  ResourceManager::createResource
+		// Access:    public 
+		// Returns:   bool
+		// Qualifier:
+		// Parameter: std::string filename
+		// Parameter: RESOURCETYPE type
+		// Parameter: size_t ID
+		// Description: Factory that creates the desired resource type. The resource can be retrieved using the ID.
+		//***********************************************************
+		virtual bool createResource(std::string filename, RESOURCETYPE::TYPE type, size_t& ID) = 0;
+		//***********************************************************
+		// Method:    reloadResource
+		// FullName:  ResourceManager::reloadResource
+		// Access:    public 
+		// Returns:   bool
+		// Qualifier:
+		// Parameter: size_t ID
+		// Description: Reloads the resource with the given ID. Reloading is for loading a resource that has already been loaded before and has since been unloaded.
+		// It will attempt to load the resource using the filename given when it was loaded the first time.
+		//***********************************************************
+		virtual bool reloadResource(size_t ID) = 0;
+		//***********************************************************
+		// Method:    unloadResource
+		// FullName:  ResourceManager::unloadResource
+		// Access:    public 
+		// Returns:   void
+		// Qualifier:
+		// Parameter: size_t ID
+		// Description: Unloads the resource with the given ID. Unloading releases the memory used back to the system.
+		//***********************************************************
+		virtual void unloadResource(size_t ID) = 0;
+		//***********************************************************
+		// Method:    removeResource
+		// FullName:  IResourceManager::removeResource
+		// Access:    virtual public 
+		// Returns:   void
+		// Qualifier:
+		// Parameter: size_t ID
+		// Description: Removes the resource completely and frees up the ID
+		//***********************************************************
+		virtual void removeResource(size_t ID) = 0;
+		virtual bool isResourceLoaded(size_t ID) = 0;
+		virtual void cleanup() = 0;
+	};
+}
 
 #endif
--- YEngine/ResourceManager.cpp
+++ YEngine/ResourceManager.cpp
@@ -3,97 +3,112 @@
 #include "Texture.h"
 
 
-std::wstring ResourceManager::convertStrToWStr(std::string str)
+namespace YEngine
 {
-	std::wstring wstr(str.length(), L' ');
-	std::copy(str.begin(), str.end(), wstr.begin());
-	return wstr;
-}
-
-bool ResourceManager::createObject(std::string filename, RESOURCETYPE::TYPE type, size_t ID)
-{
-	m_ResourceMap.insert(m_ResourceMap.end(), std::pair<size_t, Resource*>(ID, new Geometry(m_Renderer, filename, type)));
-	return m_ResourceMap[ID]->isLoaded();
-}
-
-bool ResourceManager::createTexture(std::string filename, RESOURCETYPE::TYPE type, size_t ID)
-{
-	std::wstring wfilename = this->convertStrToWStr(filename);
-	m_ResourceMap.insert(m_ResourceMap.end(), std::pair<size_t, Resource*>(ID, new Texture(m_Renderer, wfilename, type)));
-	return m_ResourceMap[ID]->isLoaded();
-}
-
-ResourceManager::ResourceManager()
-{
-	m_Renderer = nullptr;
-}
-
-void ResourceManager::init(Renderer*& renderer)
-{
-	m_Renderer = renderer;
-}
-
-bool ResourceManager::createResource(std::string filename, RESOURCETYPE::TYPE type, size_t ID)
-{
-	bool loaded = false;
-	switch (type)
+	std::wstring ResourceManager::convertStrToWStr(std::string str)
 	{
-	case RESOURCETYPE::TEXTURE:
-		loaded = this->createTexture(filename, type, ID);
-		break;
-	case RESOURCETYPE::OBJECT:
-		loaded = this->createObject(filename, type, ID);
-		break;
-	case RESOURCETYPE::FONT:
-		break;
-	case RESOURCETYPE::SOUND:
-		break;
+		std::wstring wstr(str.length(), L' ');
+		std::copy(str.begin(), str.end(), wstr.begin());
+		return wstr;
 	}
-	return loaded;
-}
-
-bool ResourceManager::reloadResource(size_t ID)
-{
-	return m_ResourceMap[ID]->reload(m_Renderer);
-}
-
-void ResourceManager::unloadResource(size_t ID)
-{
-	m_ResourceMap[ID]->unload();
-}
-
-bool ResourceManager::isResourceLoaded(size_t ID)
-{
-	return m_ResourceMap[ID]->isLoaded();
-}
-
-bool ResourceManager::getResourceData(VertexData & data, size_t ID)
-{
-	bool success = false;
-	if (m_ResourceMap[ID]->getType() == RESOURCETYPE::OBJECT) 
+	
+	bool ResourceManager::createObject(std::string filename, RESOURCETYPE::TYPE type, size_t& ID)
 	{
-		success = true;
-		data = static_cast<Geometry*>(m_ResourceMap[ID])->getData();
+		ID = m_ResourceIDs.getNewID();
+		m_ResourceMap.insert(m_ResourceMap.end(), std::pair<size_t, Resource*>(ID, new Geometry(m_Renderer, filename, type)));
+		return m_ResourceMap[ID]->isLoaded();
 	}
-	return success;
-}
-
-bool ResourceManager::getResourceData(TextureData & data, size_t ID)
-{
-	bool success = false;
-	if (m_ResourceMap[ID]->getType() == RESOURCETYPE::TEXTURE) 
+	
+	bool ResourceManager::createTexture(std::string filename, RESOURCETYPE::TYPE type, size_t& ID)
 	{
-		success = true;
-		data = static_cast<Texture*>(m_ResourceMap[ID])->getData();
+		std::wstring wfilename = this->convertStrToWStr(filename);
+		ID = m_ResourceIDs.getNewID();
+		m_ResourceMap.insert(m_ResourceMap.end(), std::pair<size_t, Resource*>(ID, new Texture(m_Renderer, wfilename, type)));
+		return m_ResourceMap[ID]->isLoaded();
 	}
-	return success;
-}
-
-void ResourceManager::cleanup()
-{
-	for (auto &i : m_ResourceMap) 
+	
+	ResourceManager::ResourceManager()
 	{
-		i.second->unload();
-		delete i.second;
+		m_Renderer = nullptr;
+	}
+	
+	void ResourceManager::init(Renderer*& renderer)
+	{
+		m_Renderer = renderer;
+	}
+	
+	bool ResourceManager::createResource(std::string filename, RESOURCETYPE::TYPE type, size_t& ID)
+	{
+		bool loaded = false;
+		switch (type)
+		{
+		case RESOURCETYPE::TEXTURE:
+			loaded = this->createTexture(filename, type, ID);
+			break;
+		case RESOURCETYPE::OBJECT:
+			loaded = this->createObject(filename, type, ID);
+			break;
+		case RESOURCETYPE::FONT:
+			break;
+		case RESOURCETYPE::SOUND:
+			break;
+		}
+		return loaded;
+	}
+	
+	bool ResourceManager::reloadResource(size_t ID)
+	{
+		return m_ResourceMap[ID]->reload(m_Renderer);
+	}
+	
+	void ResourceManager::unloadResource(size_t ID)
+	{
+		m_ResourceMap[ID]->unload();
+	}
+	
+	void ResourceManager::removeResource(size_t ID)
+	{
+		if (m_ResourceIDs.remove(ID))
+		{
+			m_ResourceMap[ID]->unload();
+			m_ResourceMap.erase(ID);
+		}
+	}
+	
+	bool ResourceManager::isResourceLoaded(size_t ID)
+	{
+		return m_ResourceMap[ID]->isLoaded();
+	}
+	
+	bool ResourceManager::getResourceData(VertexData & data, size_t ID)
+	{
+		bool success = false;
+		if (m_ResourceMap[ID]->getType() == RESOURCETYPE::OBJECT) 
+		{
+			success = true;
+			data = static_cast<Geometry*>(m_ResourceMap[ID])->getData();
+		}
+		return success;
+	}
+	
+	bool ResourceManager::getResourceData(TextureData & data, size_t ID)
+	{
+		bool success = false;
+		if (m_ResourceMap[ID]->getType() == RESOURCETYPE::TEXTURE) 
+		{
+			success = true;
+			data = static_cast<Texture*>(m_ResourceMap[ID])->getData();
+		}
+		return success;
+	}
+	
+	void ResourceManager::cleanup()
+	{
+		for (auto &i : m_ResourceMap) 
+		{
+			i.second->unload();
+			delete i.second;
+		}
+		m_ResourceIDs.clear();
 	}
 }
